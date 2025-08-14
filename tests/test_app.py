f6eda51702de61733a7bf0820da982bbe241dac5
Parminder33707 <parminder33707@gmail.com>
2025-08-14 12:07:52 +0530

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+import unittest
+from app import app
+
+class TestFitnessApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_log_workout_success(self):
+        response = self.client.post('/log_workout', json={"type": "run", "duration": 30})
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_workouts(self):
+        response = self.client.get('/get_workouts')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
